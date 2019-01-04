a5d33994dd545c8c11a6ee739a8b8dce6cd81aa1
rahisalama <46318245+rahisalama@users.noreply.github.com>
2019-01-04 22:50:53 +0300

    added comments and a dictionary for each player

---
+++ SoccerCoordinator/SoccerCoordinatorProject1.swift
@@ -0,0 +1,58 @@
+//
+//  SoccerCoordinatorProject1.swift
+//  SoccerCoordinator
+//
+//  Created by Rahi on 03/01/2019.
+//  Copyright © 2019 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+// PART1 - creating a collection named 'players' contains all 18 players information
+
+/*
+ Name               Height (inches)    Soccer Experience    Guardian Name(s)
+ Joe Smith              42                  YES             Jim and Jan Smith
+ Jill Tanner            36                  YES             Clara Tanner
+ Bill Bon               43                  YES             Sara and Jenny Bon
+ Eva Gordon             45                  NO              Wendy and Mike Gordon
+ Matt Gill              40                  NO              Charles and Sylvia Gill
+ Kimmy Stein            41                  NO              Bill and Hillary Stein
+ Sammy Adams            45                  NO              Jeff Adams
+ Karl Saygan            42                  YES             Heather Bledsoe
+ Suzane Greenberg       44                  YES             Henrietta Dumas
+ Sal Dali               41                  NO              Gala Dali
+ Joe Kavalier           39                  NO              Sam and Elaine Kavalier
+ Ben Finkelstein        44                  NO              Aaron and Jill Finkelstein
+ Diego Soto             41                  YES             Robin and Sarika Soto
+ Chloe Alaska           47                  NO              David and Jamie Alaska
+ Arnold Willis          43                  NO              Claire Willis
+ Phillip Helm           44                  YES             Thomas Helm and Eva Jones
+ Les Clay               42                  YES             Wynonna Brown
+ Herschel Krustofski    45                  YES             Hyman and Rachel Krustofski
+ 
+ */
+
+// creatibg a dictionary for each player thats contain Name, Height, Experience Level, and Guardian Name.
+
+let player1: [String: String] = ["Name": "Joe Smith", "Height": "42", "Experience": "Yes", "Guardian Name": "Jim and Jan Smith"]
+let player2: [String: String] = ["Name": "Jill Tanner", "Height": "36", "Experience": "Yes", "Guardian Name": "Clara Tanner"]
+let player3: [String: String] = ["Name": "Bill Bon", "Height": "43", "Experience": "Yes", "Guardian Name": "Sara and Jenny Bon"]
+let player4: [String: String] = ["Name": "Eva Gordon", "Height": "45", "Experience": "No", "Guardian Name": "Wendy and Mike Gordon"]
+let player5: [String: String] = ["Name": "Matt Gill", "Height": "40", "Experience": "No", "Guardian Name": "Charles and Sylvia Gill"]
+let player6: [String: String] = ["Name": "Kimmy Stein", "Height": "41", "Experience": "No", "Guardian Name": "Bill and Hillary Stein"]
+let player7: [String: String] = ["Name": "Sammy Adams", "Height": "45", "Experience": "No", "Guardian Name": "Jeff Adams"]
+let player8: [String: String] = ["Name": "Karl Saygan", "Height": "42", "Experience": "Yes", "Guardian Name": "Heather Bledsoe"]
+let player9: [String: String] = ["Name": "Suzane Greenberg", "Height": "44", "Experience": "Yes", "Guardian Name": "Henrietta Dumas"]
+let player10: [String: String] = ["Name": "Sal Dali", "Height": "41", "Experience": "No", "Guardian Name": "Gala Dali"]
+let player11: [String: String] = ["Name": "Joe Kavalier", "Height": "39", "Experience": "No", "Guardian Name": "Sam and Elaine Kavalier"]
+let player12: [String: String] = ["Name": "Ben Finkelstein", "Height": "44", "Experience": "No", "Guardian Name": "Aaron and Jill Finkelstein"]
+let player13: [String: String] = ["Name": "Diego Soto", "Height": "41", "Experience": "Yes", "Guardian Name": "Robin and Sarika Soto"]
+let player14: [String: String] = ["Name": "Chloe Alaska", "Height": "47", "Experience": "No", "Guardian Name": "David and Jamie Alaska"]
+let player15: [String: String] = ["Name": "Arnold Willis", "Height": "43", "Experience": "No", "Guardian Name": "Claire Willis"]
+let player16: [String: String] = ["Name": "Phillip Helm", "Height": "44", "Experience": "Yes", "Guardian Name": "Thomas Helm and Eva Jones"]
+let player17: [String: String] = ["Name": "Diego Soto", "Height": "42", "Experience": "Yes", "Guardian Name": "Wynonna Brown"]
+let player18: [String: String] = ["Name": "Diego Soto", "Height": "45", "Experience": "Yes", "Guardian Name": "Hyman and Rachel Krustofski"]
+
+
+
